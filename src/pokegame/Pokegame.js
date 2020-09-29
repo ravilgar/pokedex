cbd14700f4af51a0c8b7a711bf7200a528c155cc
ravilgar <ravil_g@mail.ru>
2020-09-29 22:48:45 +0300

    add interactivity

--- src/pokegame/Pokegame.js
+++ src/pokegame/Pokegame.js
@@ -1,33 +1,79 @@
 import React, { Component } from "react";
 import Pokedex from "../pokedex/Pokedex";
+import "./Pokegame.css";
+
+// Данные для приложения
+const pokemons = [
+	{ id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+	{ id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+	{ id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+	{ id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+	{ id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+	{ id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+	{ id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+	{ id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+];
+
+// функция для создания карточек игрока
+function createCards() {
+	let hand1 = [];
+	let hand2 = [...pokemons];
+	while (hand1.length < hand2.length) {
+		let randIdx = Math.floor(Math.random() * hand2.length);
+		let randPokemon = hand2.splice(randIdx, 1)[0];
+		hand1.push(randPokemon);
+	}
+	let exp1 = hand1.reduce((accum, item) => accum + item.base_experience, 0);
+	let exp2 = hand2.reduce((accum, item) => accum + item.base_experience, 0);
+	// возвращаем данные для карточек и опыт
+	return {
+		hand1: hand1,
+		hand2: hand2,
+		exp1: exp1,
+		exp2: exp2,
+	};
+}
 
 class Pokegame extends Component {
-	static defaultProps = {
-		pokemons: [
-			{ id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-			{ id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-			{ id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-			{ id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-			{ id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-			{ id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-			{ id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-			{ id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-		],
-	};
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			hand1: createCards().hand1,
+			hand2: createCards().hand2,
+			exp1: createCards().exp1,
+			exp2: createCards().exp2,
+		};
+		// привязывает this для всех экземпляров класса
+		this.giveCards = this.giveCards.bind(this);
+	}
+	// обновляем массивы для создания карточек игроков
+	giveCards() {
+		// обновляем state
+		this.setState({
+			hand1: createCards().hand1,
+			hand2: createCards().hand2,
+			exp1: createCards().exp1,
+			exp2: createCards().exp2,
+		});
+	}
+
 	render() {
-		let hand1 = [];
-		let hand2 = [...this.props.pokemons];
-		while (hand1.length < hand2.length) {
-			let randIdx = Math.floor(Math.random() * hand2.length);
-			let randPokemon = hand2.splice(randIdx, 1)[0];
-			hand1.push(randPokemon);
-		}
-		let exp1 = hand1.reduce((accum, item) => accum + item.base_experience, 0);
-		let exp2 = hand2.reduce((accum, item) => accum + item.base_experience, 0);
 		return (
 			<div>
-				<Pokedex pokemons={hand1} experience={exp1} isWinner={exp1 > exp2} />
-				<Pokedex pokemons={hand2} experience={exp2} isWinner={exp2 > exp1} />
+				<button className="Pokegame-button" onClick={this.giveCards}>
+					Try again!
+				</button>
+				<Pokedex
+					pokemons={this.state.hand1}
+					experience={this.state.exp1}
+					isWinner={this.state.exp1 > this.state.exp2}
+				/>
+				<Pokedex
+					pokemons={this.state.hand2}
+					experience={this.state.exp2}
+					isWinner={this.state.exp2 > this.state.exp1}
+				/>
 			</div>
 		);
 	}
